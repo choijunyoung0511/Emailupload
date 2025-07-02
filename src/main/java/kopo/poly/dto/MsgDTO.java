336c3a2154e166aa92969e6ea3d138860f739c7a
choijunyoung <yj766352@gmail.com>
2025-07-02 15:30:15 +0900

    Initial commit

---
+++ src/main/java/kopo/poly/dto/MsgDTO.java
@@ -0,0 +1,14 @@
+package kopo.poly.dto;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class MsgDTO {
+
+    private int result;
+    private String msg;
+}
